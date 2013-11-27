077b5b6ca12fddb1f309f147b871b701fedce397
Lauren Anderson <lmanders@snow.astro.washington.edu>
2013-11-27 12:00:49 -0800

    fixed untracked changes

--- NbodyPythonTools/analysis/makerhotplots.py
+++
@@ -1,6 +0,0 @@
-for i in range(len(outputs)):
-    output = ".".join(outputs[i].split('.')[:-1])
-    tipsy = nptipsyreader.Tipsy(output)
-    tipsy._read_param()
-    tipsy._read()
-    plots.metalstform(tipsy)                                                                                 
--- NbodyPythonTools/tools/mvfof2amiga.py
+++
@@ -1,8 +0,0 @@
-import glob
-import os
-import shutil
-
-filenames = glob.glob('*.grp')
-for i in filenames:
-    prefix = '.'.join(i.split('.')[0:3])
-    shutil.copyfile(i, prefix + '.amiga.grp')
