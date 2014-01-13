e104e994b6fcc234cc7231d73b8ddc42aaeaa8aa
Lauren Anderson <l.sonofanders@gmail.com>
2014-01-13 11:19:27 -0800

    fixed readstat to go along with rockstar

--- nbdpt/readstat.py
+++ nbdpt/readstat.py
@@ -1,14 +1,17 @@
 import numpy as np
 
-def readstat(filename):
-    statdtype = np.dtype({'names': ('grp'  ,'mvir'   ,'npart'   ,'ngas'    ,'nstar',
-                                    'ndark','gasmass','starmass','darkmass', 'rmax',
-                                    'x'    , 'y'     , 'z'      , 'vx'     , 'vy'  ,
-                                    'vz'),
-                          'formats': ('int32'  ,'float64','int32'  ,'int32'  , 'int32'  ,
-                                      'int32'  ,'float64','float64','float64', 'float64',
-                                      'float64','float64','float64','float64', 'float64',
-                                      'float64')})
-    
-    
+def readstat(filename, amiga=False):
+    if amiga: statdtype = np.dtype({'names': ('grp'  ,'npart'  ,'ngas'    ,'nstar',
+                                              'ndark','mvir','gasmass','starmass','darkmass', 'rmax',
+                                              'x'    , 'y'     , 'z'      , 'vx'     , 'vy'  ,
+                                              'vz'),
+                                    'formats': ('int32'  ,'int32','int32'  ,'int32'  , 'int32'  ,
+                                                'float64'  ,'float64','float64','float64', 'float64',
+                                                'float64','float64','float64','float64', 'float64',
+                                                'float64')})
+    else: statdtype = np.dtype({'names':('grp', 'npart', 'ngas', 'nstar', 'ndark', 
+                                         'mvir', 'rvir', 'gasmass', 'starmass', 'darkmass', 
+                                         'vmax', 'rmax', 'sigmav', 'x', 'y', 'z', 'vx', 
+                                         'vy', 'vz','contaminated', 'satellite', 'false'),
+                                'formats':tuple(5*'int32 '.split() + 14*'float64 '.split() + ['|S10', 'bool', '|S10'])})
     return np.genfromtxt(filename, dtype=statdtype, skip_header=1)
